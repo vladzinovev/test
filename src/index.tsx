098dc9e844190ba29ed01c76f5f18d9d4069c06f
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2022-12-27 17:51:12 +0300

    build project on the server

--- src/index.tsx
+++ src/index.tsx
@@ -10,13 +10,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <StoreComponent>
-      <BrowserRouter basename='/index.html'>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </StoreComponent>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
